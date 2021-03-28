059ec4de1ca88f2f5e41e5c213d217bbe5d34000
rcdong-ca <richard_dong50@hotmail.com>
2021-03-27 23:51:29 -0700

    Morse code write and read sucess

--- morsecode.c
+++ morsecode.c
@@ -8,18 +8,20 @@
 #include <linux/string.h>
 #include <linux/ctype.h>
 #include <linux/uaccess.h>
-// #include "morse_code_enc.h"
+#include <linux/kfifo.h>
+#include "morse_code_enc.h"
 
 #define MY_DEVICE_FILE "my_demo_misc"
 #define LED_DOT_TIME_ms 200
 #define LED_LINE_TIME_ms LED_DOT_TIME_ms * 3
-#define BUFF_MAXSIZE 1024
-
+#define BUFF_MAXSIZE 512
+#define FIFO_SIZE 512
+#define MY_MASK 0x8000
 static char write_buff[BUFF_MAXSIZE];
 
 //#error Are we building this file?
-/*******************Drive LEDS ********************/
 DEFINE_LED_TRIGGER(my_trigger);
+static DECLARE_KFIFO(morse_fifo, char, FIFO_SIZE);
 
 // static void my_led_blink(void) {
 //     led_trigger_event(my_trigger, LED_FULL);
@@ -45,47 +47,111 @@ static int sample_close(struct inode *inodep, struct file *filp)
 static ssize_t sample_read(struct file *file, char *buf,
 		       size_t count, loff_t *ppos)
 {
-    pr_info("Sleepy tfdfdfdime\n");
-    return 0; /* But we don't actually do anything with the data */
+    int num_bytes_read = 0;
+    pr_info("Attemping to read user buffer\n");
+    if (kfifo_to_user(&morse_fifo, buf, count, &num_bytes_read) ) {
+        printk(KERN_ERR "Unable to write to user buffer");
+        return -EFAULT;
+    }
+    return num_bytes_read; /* But we don't actually do anything with the data */
 }
 
 static ssize_t sample_write(struct file *file, const char *usr_buf,
 		       size_t len, loff_t *ppos)
 {
+
     int i;
-    char res_buffer[BUFF_MAXSIZE];  //this buffer will be the morsecode translation
+    char res_buffer[BUFF_MAXSIZE+1];  //this buffer will be the morsecode translation
     int res_count = 0;
+    int dash_count = 0;
+    int zero_count = 0;
+    int bytes_to_copy = 0;
+    short matched;
+    int bit_count =0;
+
     printk(KERN_INFO "morse_code: Flashing %d times for string.\n", len);
     //clean up the buffer
     memset(write_buff, 0, BUFF_MAXSIZE);
-    if (copy_from_user(write_buff, usr_buf + (int) *ppos, len - (int)*ppos) ) {
+    printk(KERN_INFO "ppos before = %d", (int)*ppos);
+    bytes_to_copy = ( (len  > BUFF_MAXSIZE) ? BUFF_MAXSIZE: (len ) );
+    if (copy_from_user(write_buff, usr_buf, bytes_to_copy)) {
         printk(KERN_ERR "Unable to read from buffer.");
         return -EFAULT;
     }
-    write_buff[len] = '\0';
+    //updating ppos:
+    *ppos += bytes_to_copy;
+
+    printk(KERN_INFO "ppos after = %d", (int)*ppos);
+    write_buff[bytes_to_copy+1] = '\0';
     printk(KERN_INFO "my string = %s\n", write_buff);
     //lets  now read the characters!
     for (i = 0; i< len-1  ; i++) {        //Null is a character included, skip it
         //check if the characters are letters!
         if (write_buff[i] >='a' && write_buff[i]<= 'z') {     //converts to lower case
            write_buff[i]-='a' -'A';
-           printk(KERN_INFO "char = %c\n", 'c' - 'a');
         }
         if (write_buff[i] >='A' && write_buff[i] <='Z') {
             res_buffer[res_count] = write_buff[i];
             res_count++;
-            printk(KERN_INFO "%c", write_buff[i]);
+            // printk(KERN_INFO "char2 = %c   count = %d\n", res_buffer[i], res_count);
+        }
+        else if (write_buff[i] == ' ') {
+            res_buffer[res_count] = write_buff[i];
+            res_count++;
         }
     }
 
     //rescount should now have the filtered msg, now we will to converit into morse
     //0 is space, 111 is dash
-    int dash_count = 0;
-    short matched;
     for (i = 0; i<res_count; i++) {
+        if (res_buffer[i] ==' ') {
+            kfifo_put(&morse_fifo, ' ');
+            kfifo_put(&morse_fifo, ' ');
+            kfifo_put(&morse_fifo, ' ');
+            continue;
+        }
+        matched = morsecode_codes[ (int)(res_buffer[i] - 'A') ];
+        printk(KERN_INFO "STARTING LETTER: %c\n", res_buffer[i] );
+        dash_count =0;
+        zero_count=0;
+        bit_count =0;
+        while (bit_count < sizeof(short) * 8 ) {
+            if (matched & MY_MASK) {         //get a 1
+                 //TODO: You will init the kfifo, and any correct one, we will input the morse code
+                // char into queue!!, Stop if its full, but yes!!!
+                dash_count++;
+                zero_count =0;
+                if (dash_count==3) {    //Tis a dash
+                    kfifo_put(&morse_fifo, '-');
+                    // printk(KERN_INFO " char = %c", '-');
+                    dash_count = 0;
+                }
+                // printk(KERN_INFO "bit = %d\n", 1); 
+            }
+            else {          //get a 0
+                // printk(KERN_INFO "bit = %d\n", 0); 
+                zero_count++;
+                if (zero_count==2) { //We now have trailing zeros for the character, exit the loop
+                    // pr_info("Trailing zeros detected, breaking out1");
+                    break;
+                }
+                if (dash_count ==1 ) {      //a dot
+                    dash_count = 0;
+                    kfifo_put(&morse_fifo, '.');
+                    // printk(KERN_INFO " char = %c", '.');
+                }
+            }
+            bit_count++;
+            matched = matched << 1;
+        }
+        if (i<res_count-1)
+            kfifo_put(&morse_fifo, ' ');
+        else
+            kfifo_put(&morse_fifo, '\n');
+        
         
     }
-    return len; /*return the num of bytes written*/
+    return bytes_to_copy; /*return the num of bytes written*/
 }
 
 struct file_operations sample_fops = {
@@ -111,6 +177,8 @@ static int __init testdriver_init(void) {
         "morse-code",
         &my_trigger
     );
+    INIT_KFIFO(morse_fifo);
+
     return misc_register(&sample_device);
 }
 static void __exit testdriver_exit(void){
